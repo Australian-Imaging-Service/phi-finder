ccf2e9b23667d0168882fb21c4d3144f0c7b7557
phfaustini <pedro.faustini@mq.edu.au>
2025-05-05 17:37:54 +1000

    Refactoring.

--- ml_anonymisation/dicom_tools/utils.py
+++ ml_anonymisation/dicom_tools/utils.py
@@ -8,28 +8,39 @@ from ml_anonymisation.dicom_tools import anonymise_dicom
 
 def deidentify_dicom_files(data_row: DataRow) -> None:
     """Main function to deidentify dicom files in a data row.
-    Loop over sessions in data_row.entries
-        Create a new session entry
-        Loop over images in session
-            Anonymise the image
-            Create a new name for the anonymised image
-            Save the anonymised image into the new session entry
-        Add the paths of the anonymised images to the new session entry
+        1. Download the files from the original scan entry fmap/DICOM
+        2. Anonymise those files and store the anonymised files in a temp dir
+        3. Create the deidentified entry using deid_entry = create_entry(...)
+        4. Create  a new DicomSeries object from the anonymised files dicom_series = DicomSeries('anonymised-tmp/1.dcm', ...)
+        5. Upload the anonymised files from the temp dir with deid_entry.item = dicom_series
     """
     session_keys = list(data_row.entries_dict.keys())  # Copy, not reference, of the keys, e.g. ['fmap/DICOM', 't1w/DICOM', 'dwi/DICOM']
     for session_key in session_keys:
-        anonymised_session_key = session_key.replace("/DICOM", "@deidentified")
-        anonymised_session_entry = data_row.create_entry(anonymised_session_key, datatype=DicomSeries)
+        # 1. Downloading the files from the original scan entry.
         try:
             dicom_series = data_row.entry(session_key).item
         except:
             print(f"Nothing found in data row {session_key}.")
             continue
-        anonymised_session_entry.item = DicomSeries(dicom_series.contents)
-        for dicom in data_row.entry(anonymised_session_key).item.contents:
+
+        # 2. Anonymising those files.
+        tmps_paths = []
+        for i, dicom in enumerate(dicom_series.contents):
             dcm = pydicom.dcmread(dicom)
             anonymised_dcm = anonymise_dicom.anonymise_image(dcm)
-            anonymised_dcm.save_as(dicom)
+            tmp_path = Path(f"anonymised-tmp_{i}.dcm")
+            anonymised_dcm.save_as(tmp_path)
+            tmps_paths.append(tmp_path)
+
+        # 3. Creating the deidentified entry.
+        anonymised_session_key = session_key.replace("/DICOM", "@deidentified")
+        anonymised_session_entry = data_row.create_entry(anonymised_session_key, datatype=DicomSeries)
+
+        # 4. Creating a new DicomSeries object from the anonymised files.
+        anonymised_dcm_series = DicomSeries(tmps_paths)
+
+        # 5. Uploading the anonymised files from the temp dir.
+        anonymised_session_entry.item = anonymised_dcm_series
     return data_row
 
 
