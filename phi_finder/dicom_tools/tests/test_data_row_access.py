2836d3d7936615e3438141d64acbb3e7dffeff3a
Tom Close <tom.g.close@gmail.com>
2025-05-15 13:54:31 +1000

    return None from function to be wrapped by Pydra and check for DICOM datatype

--- phi_finder/dicom_tools/tests/test_data_row_access.py
+++ phi_finder/dicom_tools/tests/test_data_row_access.py
@@ -1,8 +1,7 @@
 from pathlib import Path
 from frametree.core.row import DataRow
-import pydicom
 
-from phi_finder.dicom_tools import anonymise_dicom, utils
+from phi_finder.dicom_tools import utils
 
 
 def test_data_row_access(tmp_path: Path, data_row: DataRow) -> None:
@@ -11,16 +10,16 @@ def test_data_row_access(tmp_path: Path, data_row: DataRow) -> None:
 
 
 def test_ingest_anonymised_dicom(data_row: DataRow):
-    n_scans_before = utils._count_dicom_files(data_row, session_key=None)
+    n_scans_before = utils._count_dicom_files(data_row, resource_path=None)
     assert n_scans_before == 6
     data_row = utils.deidentify_dicom_files(data_row)
-    n_scans_after = utils._count_dicom_files(data_row, session_key=None)
+    n_scans_after = utils._count_dicom_files(data_row, resource_path=None)
     assert n_scans_after == 12
 
 
 def test_create_empty_entry(data_row: DataRow):
     # docker stop $(docker ps -aq); docker rm $(docker ps -aq)
-    key = 'fmap/DICOM'
+    key = "fmap/DICOM"
     anonymised_key = key.replace("/DICOM", "@deidentified_empty")
-    anonymised_entry = data_row.create_entry(anonymised_key, datatype=utils.DicomSeries)
-    assert 0 == utils._count_dicom_files(data_row, session_key=anonymised_key)
+    data_row.create_entry(anonymised_key, datatype=utils.DicomSeries)
+    assert 0 == utils._count_dicom_files(data_row, resource_path=anonymised_key)
